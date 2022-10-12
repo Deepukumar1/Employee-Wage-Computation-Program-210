6f8f8010722024f56730610d9166dadd55f755bf
Deepukumar1 <deepukumarmahasheth2@gmail.com>
2022-10-13 00:02:46 +0530

    swith case added

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -11,17 +11,21 @@ public class EmployeeWage {
 		int empwage=0;
 		System.out.println("Welcome to Employee Wage Computation Problem");
 		 double empcheck=(int)Math.floor(Math.random() * 10) % 3;
-	        if (empcheck==FULLTIME) {
-	            System.out.println("Employee is Present");
-	            empwage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-	            System.out.println("EmpWage: " +empwage);
-	        }else if (empcheck==PARTTIME) {
-	        	 System.out.println("Employee is Parttime");
-	             empwage=WAGE_PER_HOUR*PART_TIME_HOUR;
-	             System.out.println("EmpWage: "+empwage);
-	        }else {
-	            System.out.println("Employee is Abscent");
-	            System.out.println("EmpWage: "+empwage);
+		 switch ((int) empcheck){
+         case FULLTIME:
+         System.out.println("Employee is Present");
+         empwage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+         System.out.println("EmpWage: " + empwage);
+         break;
+         case PARTTIME:
+         System.out.println("Employee is Parttime");
+         empwage=WAGE_PER_HOUR*PART_TIME_HOUR;
+         System.out.println("EmpWage: "+empwage);
+         break;
+         default:
+     System.out.println("Employee is Abscent");
+         System.out.println("EmpWage: "+empwage);
+         break;
 	        }
 	}
 }
